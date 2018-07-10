597819240dcb94ae9a6f8ed296b6e190d3ea2613
lenq <qqcome110@gmail.com>
2018-07-10 09:58:46 +0800

    add Demo component

---
+++ src/components/Demo.vue
@@ -0,0 +1,26 @@
+<template>
+    <div class="demo-container">
+        <div class="circle-box">Demo1</div>
+    </div>
+</template>
+
+<style lang="scss" scoped>
+.circle-box {
+    width: 5vw;
+    height: 5vw;
+    border-radius: 50%;
+    background-color: #f60;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    color: #fff;
+    font-family: 'Times New Roman', Times, serif;
+}
+</style>
+
+
+<script>
+export default {
+    name: 'Demo'
+}
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,17 +1,20 @@
 <template>
   <div class="home">
     <img src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+    
+    <demo />
   </div>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator';
 import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
+import Demo from '@/components/Demo.vue'
 
 @Component({
   components: {
     HelloWorld,
+    Demo
   },
 })
 export default class Home extends Vue {}
