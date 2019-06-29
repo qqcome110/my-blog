4c8d1801a345da3c1365cd9a85610e2fe82c8113
lenq <qqcome110@gmail.com>
2019-06-29 17:34:58 +0800

    feat: set state persisted

--- src/store.ts
+++ src/store.ts
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
 import { mergeArray } from '@/utils'
+import createPersistedState from 'vuex-persistedstate'
 
 Vue.use(Vuex)
 
@@ -8,6 +9,9 @@ export default new Vuex.Store({
   state: {
     codes: []
   },
+  plugins: [
+    createPersistedState()
+  ],
   mutations: {
     setCodes (state, codes) {
       state.codes = mergeArray(state.codes, codes)
